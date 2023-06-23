b7542fe29eca3d9250e4d9842fd05468ea4e1d2b
MoryaNischay <nischaymorya@gmail.com>
2023-06-23 15:41:12 +0530

    Initial Commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return ( 
+    <div className='max-w-[1640px] mx-auto p-4'>
+        <div className='max-h-[500px] relative'>
+            {/* Overlay */}
+            <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
+                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-italic'>The <span className='text-red-700'>Best</span></h1>
+                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-italic'> <span className='text-red-700'>Food</span> Delivered</h1>
+                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-italic'>The <span className='text-red-700'>Quickest</span></h1>
+                
+            </div>
+            <img className='w-full max-h-[500px] object-cover' src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="/" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/data/data.js
@@ -0,0 +1,131 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Double Cheeseburger',
+      category: 'burger',
+      image:
+        'https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+      price: '$$$$',
+    },
+    {
+      id: 2,
+      name: 'Bacon Cheeseburger',
+      category: 'burger',
+      image:
+        'https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+      price: '$',
+    },
+    {
+      id: 3,
+      name: 'Mushroom Burger',
+      category: 'burger',
+      image:
+        'https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+      price: '$$',
+    },
+    {
+      id: 4,
+      name: 'Loaded Burger',
+      category: 'burger',
+      image:
+        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+      price: '$$$',
+    },
+    {
+      id: 5,
+      name: 'Farmhouse Pizza',
+      category: 'pizza',
+      image:
+        'http://3.bp.blogspot.com/-GEdhjxzjzVs/UpD3OFVvl8I/AAAAAAAAAQI/ZlVGqNDCvIM/s1600/Recipe-of-farmhouse-pizza-food-factory.jpg',
+      price: '$$',
+    },
+    {
+      id: 6,
+      name: 'Veggie Deluxe',
+      category: 'pizza',
+      image:
+        'https://www.killingthyme.net/wp-content/uploads/2020/09/veggie-deluxe-pizza-4.jpg',
+      price: '$$$',
+    },
+    {
+      id: 7,
+      name: 'Chicken Paradise',
+      category: 'pizza',
+      image:
+        'https://consumerist.com/consumermediallc.files.wordpress.com/2015/12/2431864_buffalochickenpizza.jpg',
+      price: '$$$$',
+    },
+    {
+      id: 8,
+      name: 'Margherita Pizza',
+      category: 'pizza',
+      image:
+        'https://naturalsolutionsmag.com/wp-content/uploads/2015/12/Pizza-Margherita.jpg',
+      price: '$',
+    },
+    {
+      id: 9,
+      name: 'Spicy Soya Chaap',
+      category: 'chaap',
+      image:
+        'https://i.ytimg.com/vi/Sj-ArGgSTDA/maxresdefault.jpg',
+      price: '$$',
+    },
+    {
+      id: 10,
+      name: 'Malai Chaap',
+      category: 'chaap',
+      image:
+        'https://spiderimg.amarujala.com/assets/images/2019/07/09/soya-malai-chaap_1562678207.jpeg',
+      price: '$$$',
+    },
+    {
+      id: 11,
+      name: 'Afghani Chaap',
+      category: 'chaap',
+      image:
+        'https://i.ytimg.com/vi/xMyqXvbVCuQ/maxresdefault.jpg',
+      price: '$$$$',
+    },
+    {
+      id: 12,
+      name: 'Mutton Chaap',
+      category: 'chaap',
+      image:
+        'http://2.bp.blogspot.com/-jVdr9qipp2s/UpX3fPU4PfI/AAAAAAAAC8k/I2D0vyaJo2I/s1600/mutton-chap-chaap-chanp-panthar-+mangsho-goat-meat-chop-ribs.jpg',
+      price: '$',
+    },
+    {
+      id: 13,
+      name: 'Buffalo Wings',
+      category: 'chicken',
+      image:
+        'https://3.bp.blogspot.com/-tRx07CaQRaQ/UXYX8V7bHNI/AAAAAAAAJ7s/Eyw2JD0hknY/s1600/BuffaloWings.jpg',
+      price: '$$',
+    },
+    {
+      id: 14,
+      name: 'Baked Chicken',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$$$',
+    },
+    {
+      id: 15,
+      name: 'Chicken Nuggets',
+      category: 'chicken',
+      image:
+        'https://3.bp.blogspot.com/-Z42uWbJ5v5U/WMJMrElKPoI/AAAAAAAARjY/FimMlci--UMMAVVH9pT6VNvgwR1G-XQrwCK4B/s1600/To%2BChicken%2BNuggets-719870.jpg',
+      price: '$',
+    },
+    {
+      id: 16,
+      name: 'Chicken Kabob',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$$$',
+    },
+  ];
+  
